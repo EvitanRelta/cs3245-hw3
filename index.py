d5a94be07061736d03aad264117eb8574aa63a3b
EvitanRelta <shauntanzongzhi@gmail.com>
2024-03-19 13:24:35 +0800

    Add Neale's 'index.py'

--- index.py
+++ index.py
@@ -3,6 +3,10 @@ import re
 import nltk
 import sys
 import getopt
+import math
+import os
+from preprocessor import Preprocessor
+from collections import Counter
 
 def usage():
     print("usage: " + sys.argv[0] + " -i directory-of-documents -d dictionary-file -p postings-file")
@@ -12,9 +16,57 @@ def build_index(in_dir, out_dict, out_postings):
     build index from documents stored in the input directory,
     then output the dictionary file and postings file
     """
+    
     print('indexing...')
     # This is an empty method
     # Pls implement your code in below
+
+
+    filenames = [int(x) for x in os.listdir(in_dir)]
+    normalized_lengths = {}
+    index = {}
+
+    for filename in sorted(filenames):
+        filepath = in_dir + str(filename)
+
+        # Each document has its own counter to keep track of the internal counts
+        counter = Counter()
+
+        # Iterate through document and maintain count
+        for token in Preprocessor.to_token_stream(filepath):
+            counter.update([token])
+
+        # Accumulate and compute normalized length
+        normalized_length = 0
+        for term_frequency in counter.values():
+            normalized_length += term_frequency ** 2
+
+        normalized_lengths[filename] = math.sqrt(normalized_length)
+        
+        # Update the index with local counts
+        for term, term_frequency in counter.items():
+            postings_list = index.get(term, [])
+            postings_list.append((filename, term_frequency))
+            index[term] = postings_list
+
+    # Write out the normalized lengths to doc_length.txt
+    with open('./doc_lengths.txt', 'w') as olen:
+        for docid, length in normalized_lengths.items():
+            olen.write(f'{docid} {length}\n')
+
+    with open(out_dict, 'w') as odict, open(out_postings, 'w') as opost:
+        start_offset = 0
+
+        # Write out the term and document frequency
+        for term, postings_list in index.items():
+            # whitespace separated (doci, term_freq)
+            opost.write(f'{" ".join([str(id_freq) for id_freq in postings_list])}\n')
+            end_offset = opost.tell()
+
+            # whitespace separated - term doc_freq start size
+            odict.write(f'{term} {len(postings_list)} {start_offset}\n')
+            start_offset = end_offset
+
 
 input_directory = output_file_dictionary = output_file_postings = None
 
